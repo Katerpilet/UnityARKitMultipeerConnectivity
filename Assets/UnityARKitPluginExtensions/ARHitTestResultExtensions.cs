0a143c18cc961505ad44f8772f00e70c9d63deab
Blake Gross <katerpilet@live.com>
2018-11-17 12:26:14 -0800

    adding extensions

---
+++ Assets/UnityARKitPluginExtensions/ARHitTestResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.XR.iOS;
+
+public static class ARHitTestResultExtensions
+{
+    public static UnityARUserAnchorData ToUnityArUserAnchorData(this ARPlaneAnchorGameObject arHitTestResult)
+    {
+        var transform = UnityARMatrixOps.GetMatrix(arHitTestResult.planeAnchor.transform);
+        var anchorData = new UnityARUserAnchorData
+        {
+            transform = transform
+        };
+        return anchorData;
+    }
+}
